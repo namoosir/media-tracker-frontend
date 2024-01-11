15d3fc0a1920b5200e40af1bea7cfd08942b2689
namoosir <saqeebnazmus@gmail.com>
2024-01-11 18:31:17 -0500

    import card and add some custom components

---
+++ src/components/youtube/TitleArrowComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+    text: string;
+}>();
+
+const text = ref(props.text);
+
+</script>
+
+<template>
+    <div class="flex justify-between items-center">
+        <p class="text-sm font-semibold leading-5">
+            {{ text }}
+        </p>
+    </div>
+</template>
--- src/components/youtube/TopChannelSummaryComponent.vue
+++ src/components/youtube/TopChannelSummaryComponent.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import TitleArrowComponent from './TitleArrowComponent.vue';
 
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center gap-3 py-3 px-5">
-    hi
+  <div class="flex flex-col justify-center items-center gap-3 py-3 px-5 rounded-md border bg-card">
+    <TitleArrowComponent text="Top Channel" />
   </div>
+
+  <!-- <Card>
+        <TitleArrowComponent text="Top Channel" />
+
+    </Card> -->
 </template>
+
